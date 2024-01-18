363156b6febb5c6bfb9ccc21f082dc8cfd14aafb
Shukla <s.rahulelite@gmail.com>
2024-01-18 17:39:44 +0530

    bs4 Done

--- BS4/EasyProject.py
+++ BS4/EasyProject.py
@@ -1,3 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+site = "https://subslikescript.com/movies"
+result = requests.get(site)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+box = soup.find("article", class_='main-article')
+titles = box.find_all('a')
+tit = []
+for title in titles:
+    tit.append(title['title'].split("'")[1])
+
+file = 'a.txt'
+with open(file, "a") as file:
+    for k in tit:
+        
+file.close()
+
