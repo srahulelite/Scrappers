87344dfa0fdcdfcb1185d7994ef106554b55b1c5
srahulelite <s.rahulelite@gmail.com>
2024-02-06 01:20:44 +0530

    pagination with scrapy Done

--- Scrappy/worldometer/worldometer/spiders/audible.py
+++ Scrappy/worldometer/worldometer/spiders/audible.py
@@ -1,6 +1,5 @@
 import scrapy
 
-
 class AudibleSpider(scrapy.Spider):
     name = "audible"
     allowed_domains = ["www.audible.com"]
@@ -15,3 +14,8 @@ class AudibleSpider(scrapy.Spider):
             yield{
                 'title':title
             }
+        pagination = response.xpath('//ul[contains(@class,"pagingElements")]')
+        next_page_url = pagination.xpath('.//span[contains(@class,"nextButton")]/a/@href').get()
+
+        if(next_page_url):
+            yield response.follow(url=next_page_url, callback=self.parse)
