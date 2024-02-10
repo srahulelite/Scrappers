5ac7b0a05c5d0abd6def2ee9e8d4930ebf6b3e79
Shukla <s.rahulelite@gmail.com>
2024-02-11 01:47:20 +0530

    xml setup done

--- Scrappy/officescrappers/officescrappers/pipelines.py
+++ Scrappy/officescrappers/officescrappers/pipelines.py
@@ -6,7 +6,7 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import pandas as pd
 
 class OfficescrappersPipeline:
     def process_item(self, item, spider):
--- Scrappy/officescrappers/officescrappers/spiders/techcrunch.py
+++ Scrappy/officescrappers/officescrappers/spiders/techcrunch.py
@@ -1,10 +1,11 @@
 import scrapy
-
+from ..pipelines import OfficescrappersPipeline
 
 class TechcrunchSpider(scrapy.Spider):
     name = "techcrunch"
     allowed_domains = ["techcrunch.com", "feeds.a.dj.com"]
     start_urls = ["https://techcrunch.com/category/fintech/feed/"]
+
 
     def start_requests(self):
         urls = [
@@ -13,13 +14,18 @@ class TechcrunchSpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
+        OfficescrappersPipeline.output_ready()
+        
     def parse(self, response):
         for post in response.xpath('//channel/item'):
             yield {
+                'origin' : response.xpath("//channel/title/text()").extract_first(),
                 'title' : post.xpath('title//text()').extract_first(),
                 'link': post.xpath('link//text()').extract_first(),
                 'pubDate' : post.xpath('pubDate//text()').extract_first(),
                 'description' : post.xpath('description//text()').extract_first(),
             }
+        
+        
 
+    
