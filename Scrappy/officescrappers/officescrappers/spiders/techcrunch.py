d8a1cc0e3e302b4458b8a8f5fb228a13e8dafef6
Shukla <s.rahulelite@gmail.com>
2024-02-11 00:14:07 +0530

    Rss Scraped

--- Scrappy/officescrappers/officescrappers/spiders/techcrunch.py
+++ Scrappy/officescrappers/officescrappers/spiders/techcrunch.py
@@ -3,17 +3,23 @@ import scrapy
 
 class TechcrunchSpider(scrapy.Spider):
     name = "techcrunch"
-    allowed_domains = ["wsj.com"]
-    start_urls = ["https://wsj.com/tech"]
+    allowed_domains = ["techcrunch.com", "feeds.a.dj.com"]
+    start_urls = ["https://techcrunch.com/category/fintech/feed/"]
+
+    def start_requests(self):
+        urls = [
+            "https://techcrunch.com/category/fintech/feed/",
+            "https://feeds.a.dj.com/rss/RSSWSJD.xml"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        front_news = response.xpath("//div[contains(@data-testid,'allesseh')]")
-        categorized_news = response.xpath("//div[@class ='css-1qsgmm']")
-        
-        for news_category in categorized_news:
-            category = news_category.xpath(".//h2[contains(@class,'eoniv5f2')]/text()").get()
-
-            yield{
-                'category': category
+        for post in response.xpath('//channel/item'):
+            yield {
+                'title' : post.xpath('title//text()').extract_first(),
+                'link': post.xpath('link//text()').extract_first(),
+                'pubDate' : post.xpath('pubDate//text()').extract_first(),
+                'description' : post.xpath('description//text()').extract_first(),
             }
 
