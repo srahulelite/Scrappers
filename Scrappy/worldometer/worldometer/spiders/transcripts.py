e5968f868162a73c75d5f4e6a111f46f0b6fce14
Shukla <s.rahulelite@gmail.com>
2024-02-12 01:57:54 +0530

    completed crawl template using user-agent, Rules, Delay, pagination

--- Scrappy/worldometer/worldometer/spiders/transcripts.py
+++ Scrappy/worldometer/worldometer/spiders/transcripts.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -6,22 +7,36 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com/movies"]
+    start_urls = ["https://subslikescript.com/movies_letter-X"]
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 
     custom_settings = {
         'DOWNLOAD_DELAY': 0.5,  # Sets a download delay of 0.5 seconds
     }
 
-
     rules = (
-        Rule(LinkExtractor(restrict_xpaths='//ul[@class="scripts-list"]/a'), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//ul[@class="scripts-list"]/a'), callback="parse_item", follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths='(//a[@rel="next"])[1]'), process_request='set_user_agent'),
     )
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.start_urls[0], 
+            headers={
+                'user-agent': self.user_agent
+            })
+        
+    def set_user_agent(self, request, spider):
+        request.headers['User-Agent'] = self.user_agent
+        return request
+
 
     def parse_item(self, response):
         article = response.xpath('//article[@class="main-article"]')
         yield{
             'title': article.xpath("./h1/text()").get(),
             'plot': article.xpath("./p/text()").get(),
-            'transcript': article.xpath("./div/text()").getall(),
+            # 'transcript': article.xpath("./div/text()").getall(),
             'url': response.url,
+            # 'user-agent': response.request.headers['User-Agent'],
         }
