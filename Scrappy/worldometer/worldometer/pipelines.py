7c45585cd0c102136db7a7e220ae155e4b3bf715
Shukla <s.rahulelite@gmail.com>
2024-02-15 23:33:17 +0530

    Data Stored into sqlite3 databased

--- Scrappy/worldometer/worldometer/pipelines.py
+++ Scrappy/worldometer/worldometer/pipelines.py
@@ -7,8 +7,42 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import logging
+import sqlite3
 
+class SQLitePipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect('transcripts.db')
+        self.c = self.connection.cursor()
 
+        #query
+        try:
+            self.c.execute('''
+                        CREATE TABLE transcripts(
+                        title TEXT,
+                        plot TEXT,
+                        transcript TEXT,
+                        url TEXT
+                           )
+                        ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def close_spider(self, spider):
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        self.c.execute('''
+            INSERT INTO transcripts (title, plot, transcript, url) VALUES(?,?,?,?)
+        ''',(
+                item.get('title'),
+                item.get('plot'),
+                item.get('transcript'),
+                item.get('url')
+            ))
+        self.connection.commit()
+        return item
+    
 class WorldometerPipeline:
     def open_spider(self, spider):
         logging.warning("SPIDER OPENED >>>>>>>")
--- Scrappy/worldometer/worldometer/settings.py
+++ Scrappy/worldometer/worldometer/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "worldometer.pipelines.AddToXlsxPipeline": 300,
+   "worldometer.pipelines.SQLitePipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
